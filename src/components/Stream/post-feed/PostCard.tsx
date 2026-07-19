f02c11a6bccc506cd55779e9cd1c03ea9fb52c5e
mohamad-ridwan <mr643062@gmail.com>
2026-07-19 15:24:37 +0700

    refactor: wrap PostCard props in Readonly utility type

--- src/components/Stream/post-feed/PostCard.tsx
+++ src/components/Stream/post-feed/PostCard.tsx
@@ -39,7 +39,7 @@ export default function PostCard({
   forceHoverComment,
   forceCopyLinkHover,
   demoLink,
-}: PostCardProps) {
+}: Readonly<PostCardProps>) {
   const hasLiked = !!post.reaction.my_reaction;
 
   return (
